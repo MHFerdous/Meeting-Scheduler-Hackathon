19f7eb228569e45a269dfe89364be53d30d3effe
Md Mahmud Hossain Ferdous <mahmudhossainferdous@gmail.com>
2024-12-08 22:32:42 +0600

    Host Create Slot

--- lib/presentation/ui/screens/host_screens/host_create_meeting_screen.dart
+++ lib/presentation/ui/screens/host_screens/host_create_meeting_screen.dart
@@ -1,21 +1,251 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task_scheduler/presentation/ui/widgets/customised_elevated_button.dart';
+import 'package:task_scheduler/presentation/ui/widgets/screen_background.dart';
+
+import '../../widgets/appbar_method.dart';
 
 class HostCreateMeetingScreen extends StatefulWidget {
   const HostCreateMeetingScreen({super.key});
 
   @override
-  State<HostCreateMeetingScreen> createState() => _HostCreateMeetingScreenState();
+  State<HostCreateMeetingScreen> createState() =>
+      _HostCreateMeetingScreenState();
 }
 
 class _HostCreateMeetingScreenState extends State<HostCreateMeetingScreen> {
+  // Define controllers for text fields and date/time pickers
+  final TextEditingController _topicTEController = TextEditingController();
+  final TextEditingController _startDateController = TextEditingController();
+  final TextEditingController _endDateController = TextEditingController();
+  final TextEditingController _startTimeController = TextEditingController();
+  final TextEditingController _endTimeController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Dispose controllers to free resources
+    _topicTEController.dispose();
+    _startDateController.dispose();
+    _endDateController.dispose();
+    _startTimeController.dispose();
+    _endTimeController.dispose();
+    super.dispose();
+  }
+
+  // Function to handle time picking
+  Future<void> _selectTime(
+      BuildContext context, TextEditingController controller) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: false),
+          child: child!,
+        );
+      },
+    );
+
+    if (pickedTime != null) {
+      setState(() {
+        controller.text = pickedTime.format(context); // Format time as 12-hour
+      });
+    }
+  }
+
+  // Function to handle date picking
+  Future<void> _selectDate(
+      BuildContext context, TextEditingController controller) async {
+    DateTime today = DateTime.now();
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: today,
+      firstDate: today, // Prevent selection of past dates
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        controller.text = "${pickedDate.year}-${pickedDate.month}-${pickedDate.day}";
+      });
+    }
+  }
+  var scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: customisedAppBar(scaffoldKey, context),
+      body: ScreenBackground(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 20.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20.h),
+              // Topic Input Field
+              TextFormField(
+                controller: _topicTEController,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(hintText: 'Topic'),
+                validator: (String? value) {
+                  if (value?.trim().isEmpty ?? true) {
+                    return 'Please enter your Topic';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 40.h),
 
-      body: Column(
-        children: [
+              // Title for Date & Time
+              Text(
+                "Date & Time",
+                style: TextStyle(fontSize: 25.sp, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20.h),
 
-        ],
+              // Start Date and Time Pickers
+              Row(
+                children: [
+                  Flexible(
+                    child: GestureDetector(
+                      onTap: () => _selectDate(context, _startDateController),
+                      child: AbsorbPointer(
+                        child: TextFormField(
+                          controller: _startDateController,
+                          decoration: const InputDecoration(
+                            labelText: "Start Date",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20.w),
+                  Flexible(
+                    child: GestureDetector(
+                      onTap: () => _selectTime(context, _startTimeController),
+                      child: AbsorbPointer(
+                        child: TextFormField(
+                          controller: _startTimeController,
+                          decoration: const InputDecoration(
+                            labelText: "Start Time",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30.h),
+
+              // "TO" Text
+              Text(
+                "TO",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.sp),
+              ),
+              SizedBox(height: 30.h),
+
+              // End Date and Time Pickers
+              Row(
+                children: [
+                  Flexible(
+                    child: GestureDetector(
+                      onTap: () => _selectDate(context, _endDateController),
+                      child: AbsorbPointer(
+                        child: TextFormField(
+                          controller: _endDateController,
+                          decoration: const InputDecoration(
+                            labelText: "End Date",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20.w),
+                  Flexible(
+                    child: GestureDetector(
+                      onTap: () => _selectTime(context, _endTimeController),
+                      child: AbsorbPointer(
+                        child: TextFormField(
+                          controller: _endTimeController,
+                          decoration: const InputDecoration(
+                            labelText: "End Time",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Time Zone - ",
+                    style: TextStyle(
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Fetch from api ",
+                    style: TextStyle(
+                        fontSize: 20.sp, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+          SizedBox(
+            height: 40.h,
+          ),
+              // Meeting Link or Address Field
+              TextFormField(
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(hintText: 'Meeting Link / Address'),
+                validator: (String? value) {
+                  if (value?.trim().isEmpty ?? true) {
+                    return 'Please enter your Meeting Link / Address';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 40.h),
+
+              // Participants Info
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Participants - ",
+                    style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+                  ),
+                  Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.w),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(8.w),
+                      child: Text(
+                        "10",
+                        style: TextStyle(fontSize: 25.sp, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40.h),
+              CustomisedElevatedButton(
+                onTap: () async {},
+                text: 'ADD',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/presentation/ui/widgets/date.dart
+++ lib/presentation/ui/widgets/date.dart
@@ -1,45 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
-class DateController extends GetxController {
-  var date = DateTime.now().obs;
+class CustomDatePicker extends StatelessWidget {
+  final TextEditingController controller;
+  final double width;
+  final double height;
+  final ValueChanged<String> onChanged;
 
-  @override
-  void onInit() {
-    super.onInit();
-    updateDate();
-  }
-
-  void updateDate() {
-    Future.delayed(const Duration(seconds: 1), () {
-      date.value = DateTime.now();
-      updateDate();
-    });
-  }
-
-  String get formattedDay => DateFormat.EEEE().format(date.value);
-}
-
-class Date extends StatelessWidget {
-  const Date({super.key});
+  const CustomDatePicker({super.key,
+    required this.controller,
+    required this.width,
+    required this.height,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GetX<DateController>(
-      init: DateController(),
-      builder: (controller) {
-        var date = controller.date.value;
-        var formattedDate = DateFormat.yMMMMd().format(date);
-        var formattedTime = DateFormat.jm().format(date);
-        var formattedDay = DateFormat.EEEE().format(date);
-        return Text(
-          "$formattedDay - $formattedDate | $formattedTime",
-          style: TextStyle(
-              fontWeight: FontWeight.w400, color: Colors.red, fontSize: 18.sp),
-        );
-      },
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextFormField(
+        controller: controller,
+        readOnly: true,
+        decoration: const InputDecoration(
+          hintText: "Select Date",
+          suffixIcon: Icon(Icons.calendar_today, color: Colors.black,),
+        ),
+        onTap: () async {
+          DateTime today = DateTime.now();
+          DateTime? pickedDate = await showDatePicker(
+            context: context,
+            initialDate: today,
+            firstDate: today, // Prevent selection of past dates
+            lastDate: DateTime(2100),
+            builder: (BuildContext context, Widget? child) {
+              return Theme(
+                data: Theme.of(context).copyWith(
+                  colorScheme: const ColorScheme.light(
+                    primary: Colors.teal, // Header background color
+                    onPrimary: Colors.white, // Header text color
+                    onSurface: Colors.black, // Body text color
+                  ),
+                  textButtonTheme: TextButtonThemeData(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.teal, // Button text color
+                    ),
+                  ),
+                ),
+                child: child!,
+              );
+            },
+          );
+
+          if (pickedDate != null) {
+            String formattedDate = "${pickedDate.year}-${pickedDate.month}-${pickedDate.day}";
+            controller.text = formattedDate;
+            onChanged(formattedDate);
+          }
+        },
+
+      ),
     );
   }
 }
